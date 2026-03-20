1ac8143e2e01b10880ecbbfed9d0cc8eaeac27eb
jefferykk77 <q1638jeffery@gmail.com>
2026-03-20 16:27:31 +0800

    ExchangeRates

---
+++ models/exchange_rate.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type ExchangeRate struct {
+	ID           uint      `gorm:"primarykey" jsom :"_id"`
+	FromCurrency string    `json:"fromcurrency" binding:"required"`
+	Tocurrency   string    `json:"toCurrency" binding:"required"`
+	Rate         float64   `json:"rate" binding:"required"`
+	Date         time.Time `json:"date"`
+}
