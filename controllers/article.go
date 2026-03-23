729d3d4a86c694b9c75768e11bf9c904fdac5536
jefferykk77 <q1638jeffery@gmail.com>
2026-03-24 01:22:22 +0800

    Redis实现获取文章

--- controllers/article.go
+++ controllers/article.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"ExchangeApp/global"
 	"ExchangeApp/models"
+	"encoding/json"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
@@ -31,18 +33,44 @@ func CreateArticle(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, article)
 }
 
+var cacheKey = "article"
+
 func GetArticle(ctx *gin.Context) {
-	var articles []models.Article
-	if err := global.Db.Find(&articles).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
-		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()}) //.Error中的结构体赋值给err，err类型为error 接口类型，接口实例自然可调用接口方法
-			//Error内有结构体，他实现了Error()方法，赋值err后，err中tab 存储了方法对应的真实底层函数地址，可直接调用
+	//从缓存中寻找
+	cacheData, err := global.RedisDB.Get(cacheKey).Result()
+	//redis中不存在该键，从数据库中寻找
+	if err == redis.Nil {
+		var articles []models.Article
+		if err := global.Db.Find(&articles).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()}) //.Error中的结构体赋值给err，err类型为error 接口类型，接口实例自然可调用接口方法
+				//Error内有结构体，他实现了Error()方法，赋值err后，err中tab 存储了方法对应的真实底层函数地址，可直接调用
+			}
+			return
 		}
+		cacheJson, err := json.Marshal(articles)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		//get声明kv对和exp
+		if err := global.RedisDB.Set(cacheKey, cacheJson, 10*time.Minute).Err(); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	} else if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
+	} else {
+		var articles []models.Article
+		if err := json.Unmarshal([]byte(cacheData), &articles); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, articles)
 	}
-	ctx.JSON(http.StatusOK, articles)
 }
 
 func GetCtxByID(ctx *gin.Context) {
